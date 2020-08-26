a0fb29d89e523a9b4bfa011d0ccb137edc5b73ef
DobriyD <41952456+DobriyD@users.noreply.github.com>
2020-08-26 22:05:59 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Shop shop-title="Smartphones"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Shop from "./components/Shop";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Shop
   }
 }
 </script>
